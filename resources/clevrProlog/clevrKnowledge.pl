811019b15542a3fe05090d24cc4f2a1b4921a8e6
sb0612 <32041616+sb0612@users.noreply.github.com>
2019-07-08 21:05:11 -0500

    added new type of question

--- resources/clevrProlog/clevrKnowledge.pl
+++ resources/clevrProlog/clevrKnowledge.pl
@@ -2,6 +2,5 @@
 object(1,cube,blue,metal,tiny,10,100).
 object(2,sphere,blue,nonmetal,tiny,70,200).
 object(3,cylinder,yellow,metal,tiny,30,290).
-object(4,sphere,pink,nonmetal,tiny,50,250).
+object(4,sphere,pink,nonmetal,medium,50,250).
 object(5,cube,red,nonmetal,medium,20,150).
-
--- resources/clevrProlog/clevrQuery.pl
+++ resources/clevrProlog/clevrQuery.pl
@@ -2,8 +2,8 @@
 :- include('clevrRules.pl').
 :- include('clevrSemanticRules.pl').
 :- include('clevrCommonFacts.pl').
-find_ans(Q) :- question(Q),find_all_filters(block, 4, L1),find_all_filters(thing, 11, L2),list_object(L1, Ids1),list_object(L2, Ids2),get_att_val(Ids1, size, Val),get_att_val(Ids2, size, Val).
-question('is the blue block the same size as the pink thing ?').
-question_answer('is the blue block the same size as the pink thing ?', true) :- find_ans('is the blue block the same size as the pink thing ?').
-question_answer('is the blue block the same size as the pink thing ?', false) :- not(find_ans('is the blue block the same size as the pink thing ?')).
+find_ans(Q) :- question(Q),find_all_filters(cylinder, 14, L1),list_object(L1, Ids1),get_att_val(Ids1, size, Val),find_all_filters(sphere, 6, L2),list_object([[size,Val]|L2], Ids2),list_subtract(Ids2, Ids1, Ids),list_length(Ids, C),quantification(N, sphere_6),gte(C, N).
+question('is there a blue nonmetal sphere that has the same size as the cylinder ?').
+question_answer('is there a blue nonmetal sphere that has the same size as the cylinder ?', true) :- find_ans('is there a blue nonmetal sphere that has the same size as the cylinder ?').
+question_answer('is there a blue nonmetal sphere that has the same size as the cylinder ?', false) :- not(find_ans('is there a blue nonmetal sphere that has the same size as the cylinder ?')).
 query(Q, A) :- question(Q),question_answer(Q, A).
